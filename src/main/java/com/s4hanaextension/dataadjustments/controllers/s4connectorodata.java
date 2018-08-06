cbedccac16e6cdc932ac331e4c106dbfd267f2eb
panigrahy.vivek.tmp@gmail.com <vivekananda.panigrahy@sap.com>
2018-08-06 12:51:51 -0400

    changes

--- src/main/java/com/s4hanaextension/dataadjustments/controllers/s4connectorodata.java
+++ src/main/java/com/s4hanaextension/dataadjustments/controllers/s4connectorodata.java
@@ -1,17 +1,24 @@
 package com.s4hanaextension.dataadjustments.controllers;
 
+
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.tomcat.util.codec.binary.Base64;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.s4hanaextension.dataadjustments.models.FileRecord;
 import com.sap.cloud.sdk.odatav2.connectivity.ODataCreateRequestBuilder;
@@ -60,6 +67,7 @@ public class s4connectorodata {
 		Map<String, Object> body = new HashMap<>();
 		body.put("IFilePath", request.get("IFilePath"));
 		body.put("IRestId", request.get("IRestId"));
+		body.put("ITimestamp", request.get("ITimestamp"));
 		
 		
 
@@ -84,13 +92,63 @@ public class s4connectorodata {
 		Map<String, Object> body = new HashMap<>();	
 		body.put("BusId", request.get("BusId"));
 		body.put("RestId", request.get("RestId"));
-		body.put("SourceGeo", request.get("SourceGeo"));
+		body.put("ProjId", request.get("ProjId"));
 		body.put("RuleId", request.get("RuleId"));
+		body.put("IRuledata", request.get("IRuleData"));
 		
 		/*FileRecord fr = new FileRecord(request.get("IFilepath").toString(), request.get("Type").toString());*/
 		
 
-		ODataCreateResult result = ODataCreateRequestBuilder.withEntity("/sap/opu/odata/sap/ZFXX_BWRES_CALL_AIF_SRV", "IMPORTSet")
+		ODataCreateResult result = ODataCreateRequestBuilder.withEntity("/sap/opu/odata/sap/ZFXX_BWRES_CALL_AIF_SRV_01", "IMPORTSet")
+						.withBodyAsMap(body)
+						.build()
+                        .execute(configContext);
+         
+		return result.asMap();
+		
+	}
+	
+	@PostMapping
+	@RequestMapping(path="triggerControlTableWorkflow")
+	public Map<String, Object> triggerControlTableWorkflow(@RequestBody List<Map<String, Object>> request) throws ODataException{
+		
+		
+		String destinationName = "glob-s4fin-sandbox_basic";
+		final ErpConfigContext configContext = new ErpConfigContext(destinationName); 
+		
+		Map<String, Object> body = new HashMap<>();	
+		
+
+		ODataCreateResult result = ODataCreateRequestBuilder.withEntity("/sap/opu/odata/sap/ZFXX_GLOBAL_CTRL_SRV", "ZFXX_GLOBAL_CTRLDetailsSet")
+						.withBodyAsMap(request.get(0))
+						.build()
+                        .execute(configContext);
+         
+		return result.asMap();
+		
+	}
+
+	
+	@PostMapping
+	@RequestMapping(path="triggerReclassificationWorkflow")
+	public Map<String, Object> triggerReclassificationWorkflow(@RequestBody Map<String, Object> request) throws ODataException{
+		
+		
+		String destinationName = "glob-s4fin-sandbox_basic";
+		final ErpConfigContext configContext = new ErpConfigContext(destinationName); 
+		
+		Map<String, Object> body = new HashMap<>();	
+		body.put("ICaccount", request.get("ICaccount"));
+		body.put("ICompcode", request.get("ICompcode"));
+		body.put("IDaccount", request.get("IDaccount"));
+		body.put("IFiscpriod", request.get("IFiscpriod"));
+		body.put("IFiscyear", request.get("IFiscyear"));
+		body.put("IProfirctr", request.get("IProfirctr"));
+		
+		/*FileRecord fr = new FileRecord(request.get("IFilepath").toString(), request.get("Type").toString());*/
+		
+
+		ODataCreateResult result = ODataCreateRequestBuilder.withEntity("/sap/opu/odata/SAP/ZFXX_LRO1_RECLASS_ACC_A_TO_B_SRV", "RuledataSet")
 						.withBodyAsMap(body)
 						.build()
                         .execute(configContext);
