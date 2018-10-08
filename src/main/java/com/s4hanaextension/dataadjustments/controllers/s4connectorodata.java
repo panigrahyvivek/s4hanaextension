074df2e0299bfbaca9d7ac9679e2d6002ea1a47a
Vivek <vivekananda.panigrahy@sap.com>
2018-10-08 12:08:37 -0400

    Changes- SKF Tool entry/Destination change test for HWE/Reclassification

--- src/main/java/com/s4hanaextension/dataadjustments/controllers/s4connectorodata.java
+++ src/main/java/com/s4hanaextension/dataadjustments/controllers/s4connectorodata.java
@@ -1,6 +1,5 @@
 package com.s4hanaextension.dataadjustments.controllers;
 
-
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -27,133 +26,151 @@ import com.sap.cloud.sdk.odatav2.connectivity.ODataException;
 import com.sap.cloud.sdk.odatav2.connectivity.ODataQueryBuilder;
 import com.sap.cloud.sdk.s4hana.connectivity.ErpConfigContext;
 
-
 @RestController
 @RequestMapping("/s4connectodata")
 public class s4connectorodata {
-	
+
 	private static Logger log = LoggerFactory.getLogger(s4connectorodata.class);
-	
-	
-	/*public s4connectorodata(){
-		String destinationName = "glob-s4fin-sandbox_basic";
-		configContext = new ErpConfigContext(destinationName); 
-	}*/
-	
-	/*@GetMapping
-	public Object checkODATAConnection() throws ODataException{
-		private final ErpConfigContext configContext;
-		String destinationName = "glob-s4fin-sandbox_basic";
-		configContext = new ErpConfigContext(destinationName); 
-		
-		final List<Object> fileRecords =
-				ODataQueryBuilder.withEntity("/sap/opu/odata/sap/ZFXX_FLATFILE_TO_ACDOCA_UPLOAD_SRV", "ZFXX_FLATFILE_TO_ACDOCASet('filepath')")
-						.select("IFilepath")
-						.build()
-                        .execute(configContext)
-                        .asList(Object.class);
-		return fileRecords;
-		
-	}*/
-	
+
+	/*
+	 * public s4connectorodata(){ String destinationName =
+	 * "glob-s4fin-sandbox_basic"; configContext = new
+	 * ErpConfigContext(destinationName); }
+	 */
+
+	/*
+	 * @GetMapping public Object checkODATAConnection() throws ODataException{
+	 * private final ErpConfigContext configContext; String destinationName =
+	 * "glob-s4fin-sandbox_basic"; configContext = new
+	 * ErpConfigContext(destinationName);
+	 * 
+	 * final List<Object> fileRecords = ODataQueryBuilder.withEntity(
+	 * "/sap/opu/odata/sap/ZFXX_FLATFILE_TO_ACDOCA_UPLOAD_SRV",
+	 * "ZFXX_FLATFILE_TO_ACDOCASet('filepath')") .select("IFilepath") .build()
+	 * .execute(configContext) .asList(Object.class); return fileRecords;
+	 * 
+	 * }
+	 */
+
 	@PostMapping
-	@RequestMapping(path="triggerFlatFlieWorkflow")
-	public Map<String, Object> triggerFlatFileWorkflow(@RequestBody Map<String, Object> request) throws ODataException{
-		
-		
+	@RequestMapping(path = "triggerFlatFlieWorkflow")
+	public Map<String, Object> triggerFlatFileWorkflow(@RequestBody Map<String, Object> request) throws ODataException {
+
 		String destinationName = "glob-s4fin-sandbox_basic";
-		final ErpConfigContext configContext = new ErpConfigContext(destinationName); 
-		
+		final ErpConfigContext configContext = new ErpConfigContext(destinationName);
+
 		Map<String, Object> body = new HashMap<>();
 		body.put("IFilePath", request.get("IFilePath"));
 		body.put("IRestId", request.get("IRestId"));
 		body.put("ITimestamp", request.get("ITimestamp"));
-		
-		
 
-		ODataCreateResult result = ODataCreateRequestBuilder.withEntity("/sap/opu/odata/sap/ZFXX_SERVER_TO_ACDOCA_SRV", "FILEPATHSet")
-						.withBodyAsMap(body)
-						.build()
-                        .execute(configContext);
-         
+		ODataCreateResult result = ODataCreateRequestBuilder
+				.withEntity("/sap/opu/odata/sap/ZFXX_SERVER_TO_ACDOCA_SRV", "FILEPATHSet").withBodyAsMap(body).build()
+				.execute(configContext);
+
 		return result.asMap();
-		
+
 	}
-	
-	
+
 	@PostMapping
-	@RequestMapping(path="triggerRestatementsWorkflow")
-	public Map<String, Object> triggerRestatementsWorkflow(@RequestBody Map<String, Object> request) throws ODataException{
-		
-		
+	@RequestMapping(path = "triggerRestatementsWorkflow")
+	public Map<String, Object> triggerRestatementsWorkflow(@RequestBody Map<String, Object> request)
+			throws ODataException {
+
 		String destinationName = "glob-s4fin-sandbox_basic";
-		final ErpConfigContext configContext = new ErpConfigContext(destinationName); 
-		
-		Map<String, Object> body = new HashMap<>();	
+		final ErpConfigContext configContext = new ErpConfigContext(destinationName);
+
+		Map<String, Object> body = new HashMap<>();
 		body.put("BusId", request.get("BusId"));
 		body.put("RestId", request.get("RestId"));
 		body.put("ProjId", request.get("ProjId"));
 		body.put("RuleId", request.get("RuleId"));
 		body.put("IRuledata", request.get("IRuleData"));
-		
-		/*FileRecord fr = new FileRecord(request.get("IFilepath").toString(), request.get("Type").toString());*/
-		
 
-		ODataCreateResult result = ODataCreateRequestBuilder.withEntity("/sap/opu/odata/sap/ZFXX_BWRES_CALL_AIF_SRV_01", "IMPORTSet")
-						.withBodyAsMap(body)
-						.build()
-                        .execute(configContext);
-         
-		return result.asMap();
-		
-	}
-	
-	@PostMapping
-	@RequestMapping(path="triggerControlTableWorkflow")
-	public Map<String, Object> triggerControlTableWorkflow(@RequestBody List<Map<String, Object>> request) throws ODataException{
-		
-		
-		String destinationName = "glob-s4fin-sandbox_basic";
-		final ErpConfigContext configContext = new ErpConfigContext(destinationName); 
-		
-		Map<String, Object> body = new HashMap<>();	
-		
+		/*
+		 * FileRecord fr = new FileRecord(request.get("IFilepath").toString(),
+		 * request.get("Type").toString());
+		 */
 
-		ODataCreateResult result = ODataCreateRequestBuilder.withEntity("/sap/opu/odata/sap/ZFXX_GLOBAL_CTRL_SRV", "ZFXX_GLOBAL_CTRLDetailsSet")
-						.withBodyAsMap(request.get(0))
-						.build()
-                        .execute(configContext);
-         
+		ODataCreateResult result = ODataCreateRequestBuilder
+				.withEntity("/sap/opu/odata/sap/ZFXX_BWRES_CALL_AIF_SRV_01", "IMPORTSet").withBodyAsMap(body).build()
+				.execute(configContext);
+
 		return result.asMap();
-		
+
 	}
 
-	
 	@PostMapping
-	@RequestMapping(path="triggerReclassificationWorkflow")
-	public Map<String, Object> triggerReclassificationWorkflow(@RequestBody Map<String, Object> request) throws ODataException{
-		
-		
+	@RequestMapping(path = "triggerControlTableWorkflow")
+	public Map<String, Object> triggerControlTableWorkflow(@RequestBody List<Map<String, Object>> request)
+			throws ODataException {
+
 		String destinationName = "glob-s4fin-sandbox_basic";
-		final ErpConfigContext configContext = new ErpConfigContext(destinationName); 
-		
-		Map<String, Object> body = new HashMap<>();	
+		final ErpConfigContext configContext = new ErpConfigContext(destinationName);
+
+		Map<String, Object> body = new HashMap<>();
+
+		ODataCreateResult result = ODataCreateRequestBuilder
+				.withEntity("/sap/opu/odata/sap/ZFXX_GLOBAL_CTRL_SRV", "ZFXX_GLOBAL_CTRLDetailsSet")
+				.withBodyAsMap(request.get(0)).build().execute(configContext);
+
+		return result.asMap();
+
+	}
+
+	@PostMapping
+	@RequestMapping(path = "triggerReclassificationWorkflow")
+	public Map<String, Object> triggerReclassificationWorkflow(@RequestBody Map<String, Object> request)
+			throws ODataException {
+
+		String destinationName = "glob-s4fes-sandbox";
+		final ErpConfigContext configContext = new ErpConfigContext(destinationName);
+
+		Map<String, Object> body = new HashMap<>();
 		body.put("ICaccount", request.get("ICaccount"));
 		body.put("ICompcode", request.get("ICompcode"));
 		body.put("IDaccount", request.get("IDaccount"));
 		body.put("IFiscpriod", request.get("IFiscpriod"));
 		body.put("IFiscyear", request.get("IFiscyear"));
 		body.put("IProfirctr", request.get("IProfirctr"));
-		
-		/*FileRecord fr = new FileRecord(request.get("IFilepath").toString(), request.get("Type").toString());*/
-		
 
-		ODataCreateResult result = ODataCreateRequestBuilder.withEntity("/sap/opu/odata/SAP/ZFXX_LRO1_RECLASS_ACC_A_TO_B_SRV", "RuledataSet")
-						.withBodyAsMap(body)
-						.build()
-                        .execute(configContext);
-         
+		/*
+		 * FileRecord fr = new FileRecord(request.get("IFilepath").toString(),
+		 * request.get("Type").toString());
+		 */
+
+		ODataCreateResult result = ODataCreateRequestBuilder
+				.withEntity("/sap/opu/odata/SAP/ZFXX_LRO1_RECLASS_ACC_A_TO_B_SRV", "RuledataSet").withBodyAsMap(body)
+				.build().execute(configContext);
+
 		return result.asMap();
-		
+
 	}
+
+	@PostMapping
+	@RequestMapping(path = "triggerSKFToolAutoWorkflow")
+	public Map<String, Object> triggerSKFToolAutoWorkflow(@RequestBody Map<String, Object> request)
+			throws ODataException {
+		String destinationName = "glob-s4fin-sandbox_basic";
+		final ErpConfigContext configContext = new ErpConfigContext(destinationName);
+		Map<String, Object> body = new HashMap<>();
+
+		body.put("Poper", request.get("Poper"));
+		body.put("Accountto", request.get("Accountto"));
+		body.put("Accountfrom", request.get("Accountfrom"));
+		body.put("Gjahr", request.get("Gjahr"));
+		body.put("Stagr", request.get("Stagr"));
+		body.put("Rldnr", request.get("Rldnr"));
+		/*
+		 * FileRecord fr = new FileRecord(request.get("IFilepath").toString(),
+		 * request.get("Type").toString());
+		 */
+
+		ODataCreateResult result = ODataCreateRequestBuilder
+				.withEntity("//sap/opu/odata/SAP/ZFXX_SKF_BRF_AUTO_TOOL_SRV", "IMPORTSet").withBodyAsMap(body).build()
+				.execute(configContext);
+
+		return result.asMap();
+	}
+
 }
